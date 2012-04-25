13f61bc051b5201108c1c550b1cbe6c655c83958
Adriano Caldeira <adriano@integgro.com.br>
2012-04-25 16:43:51 -0300

    criacao de modal e finalizacao para publicacao

---
+++ jquery.thunder/Controllers/ModalController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Jquery.Thunder.Controllers
+{
+    public class ModalController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Ajax()
+        {
+            return View("_Ajax");
+        }
+
+        public ActionResult Iframe()
+        {
+            return View("_Iframe");
+        }
+
+    }
+}
